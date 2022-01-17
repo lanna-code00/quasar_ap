cab485c0e2084667518af80388babd6a64df44ff
Mezeobidi <mezeobidi@trav4college.com>
2022-01-17 10:58:54 -0800

    added contents to the about page

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -1,6 +1,16 @@
 <template>
-   <q-page>
-       <p class="text-h5">About</p>
+   <q-page class="q-pa-lg">
+       <h4 class="q-mt-none q-mb-md">About</h4>
+        <p class="text-body1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione deleniti eaque fuga sunt atque adipisci quibusdam fugit dolorum placeat? Odit aperiam quidem consectetur sed iure cumque iusto, maiores odio quibusdam.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione deleniti eaque fuga sunt atque adipisci quibusdam fugit dolorum placeat? Odit aperiam quidem consectetur sed iure cumque iusto, maiores odio quibusdam.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione deleniti eaque fuga sunt atque adipisci quibusdam fugit dolorum placeat? Odit aperiam quidem consectetur sed iure cumque iusto, maiores odio quibusdam.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione deleniti eaque fuga sunt atque adipisci quibusdam fugit dolorum placeat? Odit aperiam quidem consectetur sed iure cumque iusto, maiores odio quibusdam.
+        </p>
+
+          <p class="text-body1">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione deleniti eaque fuga sunt atque adipisci quibusdam fugit dolorum placeat? Odit aperiam quidem consectetur sed iure cumque iusto, maiores odio quibusdam.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione deleniti eaque fuga sunt atque adipisci quibusdam fugit dolorum placeat? Odit aperiam quidem consectetur sed iure cumque iusto, maiores odio quibusdam.
+        </p>
    </q-page>
 </template>
 
