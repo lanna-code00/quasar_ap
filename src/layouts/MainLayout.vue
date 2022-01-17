cf1e952b80aad484ed48978539fec0b8ce1afa4c
Mezeobidi <mezeobidi@trav4college.com>
2022-01-17 07:49:58 -0800

    added spacings

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,27 +1,23 @@
 <template>
   <q-layout view="lHr lpR fFf">
 
-    <q-header bordered class="bg-primary text-white">
+    <q-header bordered class="text-black bg-white">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-          Title
+        <q-toolbar-title class="text-weight-bold">
+          Quasar
         </q-toolbar-title>
 
-        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" class="q-pa-md" bordered>
       <!-- drawer content -->
+      <q-icon name="fab fa-twitter" class="text-primary" size="lg"/>
     </q-drawer>
 
     <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
-      <!-- drawer content -->
     </q-drawer>
 
     <q-page-container>
