c716649282d50be28aef16931dfbbe924b4c97e0
Mezeobidi <mezeobidi@trav4college.com>
2022-01-17 10:16:34 -0800

    implemented the mobile and web logo layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -6,7 +6,8 @@
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
         <q-toolbar-title class="text-weight-bold">
-          Quasar
+          <span class="gt-sm">Quasar</span> 
+            <q-icon name="fab fa-twitter" class="header-icon q-pa-md lt-md" color="primary" size="sm"/>
         </q-toolbar-title>
 
       </q-toolbar>
@@ -14,25 +15,25 @@
 
     <q-drawer show-if-above v-model="leftDrawerOpen" side="left"  bordered :width="283">
       <!-- drawer content -->
-      <q-icon name="fab fa-twitter" class="q-pa-md" color="primary" size="lg"/>
+      <q-icon name="fab fa-twitter" class="q-pa-md gt-sm" color="primary" size="lg"/>
       <q-list>
         <q-item clickable v-ripple to="/">
            <q-item-section avatar>
-              <q-icon name="home" class="md text-weight-bold"/>
+              <q-icon name="home" class="text-weight-bold"/>
             </q-item-section>
             <q-item-section class="text-h6">Home</q-item-section>
         </q-item>
 
          <q-item clickable v-ripple to="/about">
             <q-item-section avatar>
-              <q-icon name="help" class="md text-weight-bold"/>
+              <q-icon name="help" class="text-weight-bold"/>
               </q-item-section>
             <q-item-section class="text-h6">About</q-item-section>
          </q-item>
 
           <q-item clickable v-ripple to="/settings">
             <q-item-section avatar>
-              <q-icon name="fas fa-cog" class="md text-weight-bold"/>
+              <q-icon name="fas fa-cog" class="text-weight-bold"/>
               </q-item-section>
             <q-item-section class="text-h6">Setting</q-item-section>
           </q-item>
@@ -106,3 +107,11 @@ export default {
   }
 }
 </script>
+
+<style lang="sass">
+  .header-icon
+     position: absolute
+     left: 50%
+     bottom: 0
+     transform: translateX(-50%)
+</style>
